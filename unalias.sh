ff43eb5f3b44b2c6f03a52653b604d9602ad869c
liugnggh71 <liugng71@gmail.com>
2020-11-03 22:20:50 -0600

    Create unalias.sh

---
+++ unalias.sh
@@ -0,0 +1,17 @@
+unalias F1
+unalias F2
+unalias F
+unalias H
+unalias dirs
+unalias act
+unalias conf
+unalias repo
+unalias 600
+unalias 700
+unalias 7445
+unalias perf
+unalias 1h
+unalias status
+unalias restart
+unalias start
+unalias stop
